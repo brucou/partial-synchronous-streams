77391ecea201b4d2a1384f955b3ac1cebe3e2f88
brucou <bruno.couriol@gmail.com>
2017-07-06 04:15:40 +0200

    wrote combine combinator

--- src/partial_synchronous_streams.js
+++ src/partial_synchronous_streams.js
@@ -69,6 +69,12 @@ const { INIT, NEW, SAME, ERROR, DONE, TEMP } = STATES;
 
 export const ERROR_FROM_GENERATING_BEHAVIOR = `Error occured while observing on behaviour stream! `;
 
+function pull(iterator) {
+  iterator.pull();
+
+  return iterator.get()
+}
+
 function makePullMessage() {
   return { [PULL_EVENT]: null }
 }
@@ -90,13 +96,13 @@ export function fromFn(fn, settings) {
   let _value = automaton.start();
 
   return {
-    get: function () {
+    get: function fromFnGet() {
       return {
         controlState: _value.controlState,
         output: _value.output
       }
     },
-    pull: function () {
+    pull: function fromFnPull() {
       _value = automaton.yield(makePullMessage())
     }
   }
@@ -113,49 +119,50 @@ export function fromBehavior(behavior, settings) {
   );
 
   return {
-    get: function () {
+    get: function fromBehaviorGet() {
       return {
         controlState: _value.controlState,
         output: _value.output
       }
     },
-    pull: function () {
+    pull: function fromBehaviorPull() {
       _value = automaton.yield(makePullMessage())
     }
   }
 }
 
 export function map(pss, fn) {
-  let cachedValue = null
+  let cachedValue = null;
 
   return {
-    get: function () {
+    get: function mapGet() {
       return cachedValue
     },
-    pull: function () {
-      pss.pull()
+    pull: function mapPull() {
+      pss.pull();
       const { controlState, output } = pss.get();
 
-      // TODO : add case where output == NO_OUTPUT!! must be mapped to NO_OUTPUT
-      // TODO : map should return also {controlState, output} !! so no good here (imagine two maps)
-      // Note that map does not change the control state, just the output
+      // NOTE : At the difference of 'atomic' iterators, the map combinator does not treat the
+      // DONE and ERROR as absorbing states. That means that after returning an error as output,
+      // map could possibly return a valid value as output, if the source iterator does so.
+      // It is yet an open matter whether this is the correct default behavior.
       switch (controlState) {
         case INIT :
           // could happen for instance with fromFn, when one does a get before a pull (!! dont
-          // do that!!) In such pathologica case, we just silently output nothing
+          // do that!!) In such pathological case, we just silently output nothing
           // TODO : put in doc
-          cachedValue = { controlState, output : NO_OUTPUT};
+          cachedValue = { controlState, output: NO_OUTPUT };
           break;
         case NEW :
           cachedValue = {
             controlState,
-            output : equals(output, NO_OUTPUT) ? NO_OUTPUT : fn(output)
+            output: equals(output, NO_OUTPUT) ? NO_OUTPUT : fn(output)
           }
           break;
         case SAME :
           cachedValue = {
-            controlState : SAME,
-            output : cachedValue.output
+            controlState: SAME,
+            output: cachedValue.output
           }
           break;
         case ERROR :
@@ -164,6 +171,69 @@ export function map(pss, fn) {
         case DONE :
           cachedValue = { controlState, output }
           break;
+      }
+    }
+  }
+}
+
+function hasOneWitnError(iteratorsOutput) {
+  return iteratorsOutput.some(outputStruct => outputStruct.controlState === ERROR)
+}
+
+function hasOneWitnDone(iteratorsOutput) {
+  return iteratorsOutput.some(outputStruct => outputStruct.controlState === DONE)
+}
+
+function hasOneWitnNoOutput(iteratorsOutput) {
+  return iteratorsOutput.some(
+    outputStruct =>
+      (outputStruct.controlState === NEW && equals(outputStruct.output, NO_OUTPUT)) ||
+      (outputStruct.controlState === SAME && equals(outputStruct.output, NO_OUTPUT))
+  )
+}
+
+function hasAllSame(iteratorsOutput) {
+  return iteratorsOutput.every(outputStruct => outputStruct.controlState === SAME)
+}
+
+export function combine(combiningFn, arrayOfIterators) {
+  let cachedValue = null;
+
+  return {
+    get: function combineGet() {
+      return cachedValue
+    },
+    // NOTE : At the difference of 'atomic' iterators, the combin combinator does not treat the
+    // DONE and ERROR as absorbing states. That means that after returning an error as output,
+    // map could possibly return a valid value as output, if the source iterator does so.
+    // It is yet an open matter whether this is the correct default behavior.
+    pull: function combinePull() {
+      const iteratorsOutput = arrayOfIterators.map(pull);
+
+      if (hasOneWitnError(iteratorsOutput)) {
+        // Concatenate all errors with their indices
+        cachedValue = {controlState : ERROR, ouput : aggregateIteratorsError(iteratorsOutput)};
+      }
+      else if (hasOneWitnDone(iteratorsOutput)) {
+        cachedValue = {controlState : DONE, ouput : NO_OUTPUT};
+      }
+      else if (hasOneWitnNoOutput(iteratorsOutput)) {
+        // NOTE : I could also memoize the aggregatedCcontrolState and return SAME for second pull
+        // bit I don't. I don't foresee any use for such distinction downstream.
+        cachedValue = {controlState : NEW, ouput : NO_OUTPUT};
+      }
+      else if (hasAllSame(iteratorsOutput)) {
+        cachedValue = {controlState : SAME, output : cachedValue.output};
+      }
+      else {
+        // reaching here means all iterators have an non-zero output with control state either
+        // NEW or SAME. Hence we can execute the combining function on that
+        const combiningFnArgsArray = iteratorsOutput.map((obj) => obj.output);
+
+        cachedValue = {
+          controlState : NEW,
+          output : combiningFn.apply(combiningFn, combiningFnArgsArray)
+        };
       }
     }
   }
